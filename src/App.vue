c7bbe001c2384de93faa4defc6f9c6e6465732ac
yaxun <weicaixun@Castle-in-the-Sky.local>
2018-12-11 15:24:55 +0800

    完善屏幕适配, 导航条滚动和点击动画功能

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Navtop></Navtop>
   </div>
 </template>
 
 <script>
+import Navtop from './components/navtop/navtop'
 
 export default {
   name: 'App',
+  mounted: function () {
+    window.onresize = function () {
+      let width = document.body.clientWidth
+      let fontSize = 0
+      if (width < 518) { // 小
+        fontSize = 18
+      } else if (width < 960) { // 中
+        fontSize = 20
+      } else { // 大
+        fontSize = 23
+      }
+      if (document.documentElement.style.fontSize === fontSize) { return }
+      document.documentElement.style.fontSize = fontSize + 'px'
+    }
+  },
+  components: {
+    Navtop
+  }
 }
 
 </script>
 
 <style lang="stylus">
+  #app
+    width 100%
+    background-color blue
+
 </style>
--- src/components/navtop/navtop.vue
+++ src/components/navtop/navtop.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="nav-wrapper">
     <div class="top">
-      <h1 class="logo">
-        <img class='logo-icon' src='./imgs/logo.png'/>
+      <h1 class="nav-icon">
+        <img src='./imgs/logo.png'/>
       </h1>
       <div class="search">
         <a class="search-button">
@@ -10,7 +10,7 @@
           <span>大家都在搜: 吴磊 我站在这里好吗</span>
         </a>
       </div>
-      <a class="edit-weibo">
+      <a class="nav-icon">
         <img src='./imgs/edit.svg'/>
       </a>
     </div>
@@ -18,10 +18,9 @@
     <div class="bottom">
       <div class="navlist-wrapper">
           <ul class="navlist-content" ref='scroll' :style='{width: contentWidth}'>
-            <li v-for='item, index in list' :key='index' class="navlist-item">
-              <a class="item-button">
-                {{item}}
-              </a>
+            <li v-for='(item, index) in list' :key='index' :class='{"navlist-item": true, "navlist-item-select": index === selectIndex}'
+            @click="_changeSelect(index)">
+              <a class="item-button">{{item}}</a>
             </li>
           </ul>
       </div>
@@ -32,60 +31,79 @@
   </div>
 </template>
 
-
 <script>
 
-import BScroll from 'better-scroll';
+import BScroll from 'better-scroll'
 
 export default {
   name: 'navtop',
-  data() {
+  data () {
     return {
       list: ['热门', '新鲜事', '搞笑', '情感', '明星', '社会', '数码', '体育', '汽车', '电影', '游戏'],
-      contentWidth: '1000px'
+      contentWidth: '100px',
+      selectIndex: 0,
+      screenWidth: document.body.clientWidth
     }
   },
-  mounted() {
-    //1. 计算出滚动的size
-    let width = 0
-    Array.from(this.$refs.scroll.children).forEach((element)=>{
-      width += element.clientWidth
-      console.log('width', element.clientWidth)
-    })
-    this.contentWidth = width + 'px'
-    console.log(this.contentWidth)
-
-    this.$nextTick(() => {
-      new BScroll('.navlist-wrapper', {
-          click: true,
-          scrollX: true
-        });
+  methods: {
+    _changeSelect: function (index) {
+      this.selectIndex = index
+      let elment = Array.from(this.$refs.scroll.children)[index]
+      // 判断是否需要滚到中间去
+      this.scroll.scrollToElement(elment, 1000, true)
+    },
+    _recaculateContentWidth: function () {
+      let width = 0
+      Array.from(this.$refs.scroll.children).forEach((element) => {
+        width += (element.clientWidth)
       })
-  }
 
+      this.contentWidth = (width + 1) + 'px'
+      console.log(this.contentWidth)
+    }
+  },
+  mounted () {
+    this._recaculateContentWidth()
+    this.$nextTick(() => {
+      this.scroll = new BScroll('.navlist-wrapper', { click: true, scrollX: true })
+    })
+    var that = this
+    window.addEventListener('resize', () => {
+      let fontSize = document.documentElement.style.fontSize
+      if (that.lastFontSize === fontSize) { return }
+      that.lastFontSize = fontSize
+      that._recaculateContentWidth()
+    })
+  }
 }
 </script>
 
-<style scope lang="stylus">
+<style lang="stylus" scope>
 .nav-wrapper
   position fixed
   top 0
   left 0
+  right 0
   width 100%
+  max-width 960px
+  min-width 320px
+  margin 0 auto
+  background-color white
+  height 5.7rem
   .top
     display flex
     flex-direction row
-    height 2.3rem
+    height 2.9rem
     justify-content space-between
     align-items center
-    .logo
+    .nav-icon
       height 100%
-      width 1.3rem
+      width 1.6rem
       display flex
       align-items center
-      padding-left 0.6rem
-      padding-right 0.6rem
-      .logo-icon
+      padding-left 0.75rem
+      padding-right 0.75rem
+      img
         width 100%
     .search
       width 100%
@@ -93,13 +111,12 @@ export default {
       box-sizing border-box
       display flex
       align-items center
-      padding-left 0.5rem
-      padding-right 0.5rem
+      padding-left 0.625rem
+      padding-right 0.625rem
       .search-button
-        display inline-block
         background-color #ebebec
         width 100%
-        height 1.4rem
+        height 1.75rem
         display flex
         flex-direction space-between
         justify-content flex-start
@@ -108,26 +125,15 @@ export default {
           height 60%
           padding 0 0.5rem
         span
-          font-size 0.8rem
           color #bdbdbd
           overflow hidden
           white-space nowrap
           text-overflow ellipsis
           width 100%
-          font-size 0.6rem
-          line-height 100%
-    .edit-weibo
-      height 100%
-      width 1.3rem
-      padding-left 0.6rem
-      padding-right 0.6rem
-      display flex
-      align-items center
-      img
-        width 100%
+          font-size 0.8rem
   .bottom
     position relative
-    height 2.2rem
+    height 2.8rem
     .navlist-wrapper
       height 100%
       width 100%
@@ -136,18 +142,35 @@ export default {
       box-sizing border-box
       .navlist-content
         height 100%
+        overflow hidden
         padding-right 2.2rem
         .navlist-item
           list-style none
           height 100%
           float left
           display block
-          padding 0 0.6rem
+          padding 0 0.75rem
           color #a5adb5
-          font-size 0.8rem
+          font-size 1rem
           line-height 44px
+          cursor pointer
         .navlist-item:active
+          .item-button
+            background-color #bdbdbd
+        .navlist-item-select
           color #262626
+          position relative
+        .navlist-item-select:after
+          content ''
+          float left
+          bottom 0
+          height 2px
+          left 50%
+          margin-left -0.5rem
+          width 1rem
+          position absolute
+          background-color #262626
+
     .navlist-indicator-button
       position absolute
       right 0
